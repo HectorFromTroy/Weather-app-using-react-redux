c79cff5525f22370d00a78efd6d4fa87431929e8
HectorFromTroy <niaz.nazipoff@yandex.ru>
2020-08-27 16:24:45 +0300

    added separate component for loading icon

---
+++ src/components/LoadingIcon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoadingIcon = props => {
+  return (
+    <div className="loadingIcon">
+      <div className="stick"></div>
+      <div className="stick"></div>
+      <div className="stick"></div>
+      <div className="stick"></div>
+    </div>
+  );
+}
+
+export default LoadingIcon
